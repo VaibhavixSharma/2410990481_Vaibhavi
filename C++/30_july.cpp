4689a69eda0c0455a927dabf749260fe4c729617
Vaibhavi <vaibhavixsharma@gmail.com>
2025-07-31 10:44:24 +0530

    Update 30_july.cpp

--- C++/30_july.cpp
+++ C++/30_july.cpp
@@ -1,6 +1,17 @@
+// LAMBDA FUNCTION 
+/*
+it is a inline, quick, unnamed fxn for quick jobs. 
+Use capture list [] to access outside variabes.
+[x] --> captures by value. [&x] --> captures by reference.
+() ---> to pass arguments
+*/
+
 #include <iostream>
 using namespace std;
 int main(){
-    
+    auto sayhello = [](){
+    cout<<"Hello\n";
+};
+    sayhello();
     return 0;
 }
